60d5699f3b36cd5e3b1da6f8f231539445d2d7b1
omarfrt <iroonix5@gmail.com>
2019-09-17 12:44:37 +0100

    get tested nigga

--- api/models/order.js
+++ api/models/order.js
@@ -4,16 +4,16 @@ const orderSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   products: [{  _id:{type: String, required: true},
     bookname: {type: String, required: true},
-    hot:{type:Boolean , required : true},
+    hot:{type:Boolean , required : false},
     aboutbook: {type: String, required: true},
     authorname: {type: String, required: true},
     isbn: {type: String, required: true},
     genre: {type: String, required: true},
    // quantity:Number,
-    price: Number,
-    rating:Number,
-    productimgl:{type: String, required: true},
-    productimgs:{type: String, required: true}
+    price: {type: Number, required: true},
+    rating:{type: Number, required: false},
+    productimgl:{type: String, required: false},
+    productimgs:{type: String, required: false}
   }],
   totalPrice: {type:Number, required:true}
 });
