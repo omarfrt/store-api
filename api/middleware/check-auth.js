683635d1170e4e7023e0a17a49636804715f5389
omarfrt <iroonix5@gmail.com>
2019-11-02 15:51:21 +0100

    nothing

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -2,7 +2,6 @@
 const jwt = require('jsonwebtoken');
 const pwdjwt= 'secret'
 
-
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]; // to take off bearer
