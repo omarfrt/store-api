5fdf1480f973a2dd5edbef28f38b789c4e73ea36
omarfrt <iroonix5@gmail.com>
2019-12-12 17:19:52 +0100

    getting buyer info

--- api/routes/buyers.js
+++ api/routes/buyers.js
@@ -10,7 +10,7 @@ const Buyer = require ('../models/buyer');
 const checkAuth = require('../middleware/buyer_auth');
 
 
-router.get('/info',checkAuth,(req,res,next)=>{
+router.post('/info',checkAuth,(req,res,next)=>{
    Buyer.find({email:req.body.email})
    .then(docs=>{
       const response = {
@@ -44,44 +44,6 @@ router.get('/info',checkAuth,(req,res,next)=>{
 
 
 
-router.post('/info',checkAuth,(req,res,next)=>{
- const buyer = new Buyer({
-    _id:  new mongoose.Types.ObjectId(),
-    firstname:req.body.firstname,
-    lastname: req.body.lastname,
-    email:req.body.email,
-    phone:req.body.phone,
-    address:req.body.address,
-    cin:req.body.cin
- });
-
-buyer.save()
-.then(result => {
-   res.status(201).json({
-       message:'buyer info saved',
-       buyer_info:{
-         _id: result._id,
-         firstname : result.firstname,
-         lastname: result.lastname,
-         email: result.email,
-         phone: result.phone,
-         address: result.address,
-         cin: result.cin
-       },
-       request:{
-         type:'get',
-         url:  'http://localhost:3000/buyers/'+ result._id
-       }
-   });
-
- })
- .catch(err=>{
-   console.log(err);
-   res.status(500).json({
-     error:err
-   });
- });
-});
 
 
 
